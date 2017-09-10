ba570d0cae7687a4450ea7878a99524599773db5
arellaTV <jeremiah.arella@gmail.com>
2017-09-09 21:50:04 -0700

    Uses react to move character to the right of the screen

--- src/Game/Prototype01/index.js
+++ src/Game/Prototype01/index.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
+import Walker from './walker';
 
 class Prototype01 extends Component {
-  componentDidMount() {
+  constructor() {
+    super();
     this.app = new window.PIXI.Application(1280, 720);
+
+    this.state = {
+      position: {
+        x: 200,
+        y: 400,
+      },
+    };
+  }
+
+  componentDidMount() {
     this.gameCanvas.appendChild(this.app.view);
     this.app.start();
+    this.moveRight();
   }
 
   componentWillUnMount() {
     this.app.stop();
   }
 
+  moveRight() {
+    const interval = setInterval(() => {
+      if (this.state.position.x > 1080) {
+        clearInterval(interval);
+      }
+
+      this.setState({
+        position: {
+          x: this.state.position.x + 10,
+          y: this.state.position.y,
+        },
+      });
+    }, 16);
+  }
+
   render() {
     return (
-      <div ref={thisDiv => this.gameCanvas = thisDiv} />
+      <div ref={thisDiv => this.gameCanvas = thisDiv}>
+        <Walker
+          app={this.app}
+          position={this.state.position}
+          scale={0.25}
+        />
+      </div>
     );
   }
 }
---
+++ src/Game/Prototype01/walker.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import bind from 'react-autobind';
+
+class Walker extends Component {
+  constructor(props) {
+    super(props);
+    this.walker = window.PIXI.Sprite.fromImage('assets/sprites/mario.png');
+    this.walker.anchor.set(0.5);
+    this.walker.scale.y = 0.25;
+    this.walker.scale.x = 0.25;
+    console.log(this.walker.scale);
+    bind(this);
+  }
+
+  componentDidMount() {
+    const app = this.props.app;
+    console.log('app', app);
+    app.stage.addChild(this.walker);
+  }
+
+  setPosition(x, y) {
+    this.walker.x = x;
+    this.walker.y = y;
+  }
+
+  setScale(scale) {
+    this.walker.scale.x = scale;
+    this.walker.scale.y = scale;
+  }
+
+  render() {
+    console.log('walker rendering');
+    const x = this.props.position.x;
+    const y = this.props.position.y;
+    const scale = this.props.scale;
+    this.setPosition(x, y);
+    this.setScale(scale);
+    return null;
+  }
+}
+
+export default Walker;
