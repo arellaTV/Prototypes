8b8d514dc6d5587ff96f3eb9fc3afc7099cf59c0
arellaTV <jeremiah.arella@gmail.com>
2017-09-16 16:59:50 -0700

    Decouples Game instance from settings

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,5 +1,3 @@
-import './settings'
-
 const Game = new window.PIXI.Application(1280, 720);
 
 export default Game;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './Game'
+import './Game/settings'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
